551de4fe1b69e7871d34ec8bfa1ef2d3dd28a59e
Syntthetix <grose11@students.kennesaw.edu>
2024-05-15 22:40:22 -0400

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -7,6 +7,11 @@ print("  RAT PORT SCANNER")
 print("-" * 20)
 print()
 
+if len(sys.argv) != 4:
+    print("Error: Missing arguments!")
+    print("Usage: portscan.py [target] [start_port] [end_port]")
+    sys.exit()
+
 target = sys.argv[1]
 
 print("Scanning target: " + target)
