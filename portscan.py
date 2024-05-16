9a9af8c613799bd1c2e5798bbc01b3040d6ef4d0
Garrett <garrett16r@gmail.com>
2024-05-15 23:00:24 -0400

    Fix variable definition location

--- portscan.py
+++ portscan.py
@@ -12,6 +12,10 @@ if len(sys.argv) == 3:
     print("pyscan.py [target] (Defaults to all ports)")
     sys.exit()
 
+target = sys.argv[1]
+start_port = 1
+end_port = 65535
+
 # Set start_port and end_port to user-defined values if provided
 if (len(sys.argv) == 4):
     start_port = sys.argv[2]
@@ -19,10 +23,6 @@ if (len(sys.argv) == 4):
     if (int(end_port) > 65535):
         print("Error: end_port cannot be greater than 65535!")
         sys.exit()
-
-target = sys.argv[1]
-start_port = 1
-end_port = 65535
 
 print("Scan target:\t" + target)
 print("Target ports:\t{} -> {}".format(start_port, end_port))
