a543310ef9b707d452682678be1733ec36aa9227
Garrett <garrett16r@gmail.com>
2024-05-15 22:56:24 -0400

    Updated pyscan.py

--- portscan.py
+++ portscan.py
@@ -5,14 +5,21 @@ from datetime import datetime
 print("-" * 20)
 print("  RAT PORT SCANNER")
 print("-" * 20)
-print()
 
-if len(sys.argv) != 4:
+if len(sys.argv) == 3:
     print("Error: Missing arguments!")
-    print("Usage: portscan.py [target] [start_port] [end_port]")
+    print("Usage: portscan.py [target] [start_port] <end_port>")
+    print("pyscan.py [target] (Defaults to all ports)")
     sys.exit()
 
-target = sys.argv[1]
+if (len(sys.argv) == 4):
+    start_port = sys.argv[2]
+    end_port = sys.argv[3]
 
-print("Scanning target: " + target)
-print("Scan started at: " + str(datetime.now()))
+target = sys.argv[1]
+start_port = 1
+end_port = 65535
+
+print("Scan target:\t" + target)
+print("Target ports:\t{} -> {}".format(start_port, end_port))
+print("Started at:\t" + str(datetime.now()))
