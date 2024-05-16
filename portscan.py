9c13dd3c00a729e42616df5f1fbd5098d7e59841
Garrett <garrett16r@gmail.com>
2024-05-15 22:58:32 -0400

    Add end_port error handling

--- portscan.py
+++ portscan.py
@@ -12,9 +12,13 @@ if len(sys.argv) == 3:
     print("pyscan.py [target] (Defaults to all ports)")
     sys.exit()
 
+# Set start_port and end_port to user-defined values if provided
 if (len(sys.argv) == 4):
     start_port = sys.argv[2]
     end_port = sys.argv[3]
+    if (end_port > 65535):
+        print("Error: end_port cannot be greater than 65535!")
+        sys.exit()
 
 target = sys.argv[1]
 start_port = 1
